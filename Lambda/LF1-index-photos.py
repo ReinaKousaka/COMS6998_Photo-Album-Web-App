9721d9425e0276bbb5d2530c2f3aafacb4a5da1a
ReinaKousaka <songheng.yin@mail.utoronto.ca>
2023-03-23 11:27:17 -0400

    add files

---
+++ Lambda/LF1-index-photos.py
@@ -0,0 +1,98 @@
+'''Documentation:
+opensearchpy: https://opensearch-project.github.io/opensearch-py/api-ref/clients/opensearch_client.html#opensearchpy.OpenSearch.index
+'''
+import boto3
+import json
+from requests_aws4auth import AWS4Auth
+from opensearchpy import OpenSearch, RequestsHttpConnection
+from datetime import *
+
+# OpenSearch
+REGION = 'us-east-1'
+HOST = 'search-photos-u5cripkrzbx3hj72iep5ylr5ai.us-east-1.es.amazonaws.com'
+INDEX = 'photos'
+
+s3_client = boto3.client('s3')
+rek_client = boto3.client('rekognition')
+
+
+def get_labels(bucket, key):
+    response = rek_client.detect_labels(
+        Image={
+            'S3Object': {
+                'Bucket': bucket,
+                'Name': key
+            }
+        },
+        MaxLabels=10
+    )
+    labels = list(map(lambda x: x['Name'], response['Labels']))
+    print(f'labels is {labels}')
+    return labels
+
+
+def retrieve_metadata(bucket, key):
+    response = s3_client.head_object(Bucket=bucket, Key=key)
+    print(f'head_object res is {response}')
+    customlabels = []
+    if response["Metadata"]:
+        # TODO: check correctness of customlabels
+        customlabels = response["Metadata"]["customlabels"]
+        print(f'customlabels: {customlabels}')
+        customlabels = customlabels.split(',')
+        customlabels = list(map(lambda x: x.lower(), customlabels))
+    return customlabels
+
+
+def store_open_search(json_data):
+    def get_awsauth(region, service):
+        cred = boto3.Session().get_credentials()
+        return AWS4Auth(
+            cred.access_key,
+            cred.secret_key,
+            region,
+            service,
+            session_token=cred.token
+        )
+    
+    opensearch_client = OpenSearch(
+        hosts = [{'host': HOST, 'port': 443}],
+        http_auth = get_awsauth(REGION, 'es'),
+        use_ssl = True,
+        verify_certs = True,
+        connection_class = RequestsHttpConnection
+    )
+    try:
+        response = opensearch_client.index(
+            index='photos',
+            body=json.dumps(json_data)
+        )
+        print(f'response is {response}')
+        return response
+    except Exception as e:
+        print(f'failed to upload to OpenSearch: {e}')
+    
+
+def lambda_handler(event, context):
+    # for record in event['Records']:
+    record = event['Records'][0]
+
+    print(f'record is {record}')
+    bucket = record['s3']['bucket']['name']
+    key = record['s3']['object']['key']
+    createdTimestamp = record['eventTime']
+
+    labels = get_labels(bucket, key)
+    customlabels = retrieve_metadata(bucket, key)
+
+    store_open_search({
+        'objectKey': key,
+        'bucket': bucket,
+        'createdTimestamp': createdTimestamp,
+        'labels': labels + customlabels
+    })
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('LF1 triggered')
+    }
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# COMS6998_Photo-Album-Web-App
+# COMS6998_Photo Album Web App
+### Overview
+This is the Assignment 2 for course COMS6998: Cloud Computing and Big Data at Columbia University.
+The task is to implement a photo album web application that can be searched using natural language through both text and voice.
+
+### Deployed S3 Frontend Link
+
+### Architecture
+The VPC is not implemented.
+![](Images/architecture.png)
+
+### Team Members
+- Songheng Yin, sy3079
+- Yiming Zhu, yz4336
